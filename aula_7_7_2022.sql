3662b47eb06cbb99c40c0808d962bad6da3f609f
Flávio Santos <96176081+Flaviolsantos@users.noreply.github.com>
2022-07-07 12:00:37 +0000

    Update aula_7_7_2022.sql

--- aula_7_7_2022.sql
+++ aula_7_7_2022.sql
@@ -10,3 +10,19 @@ select CategoryID, avg(UnitPrice) from Products group by CategoryID;
 select CategoryID, avg(UnitPrice) from Products where CategoryID = 1;
 
 select * from Products where UnitPrice > 37.97916667 and CategoryID = 1;
+
+
+
+--------------------------------------------------------
+
+-- 1 - quem são os chefes de todos os funcionários
+select B.EmployeeID, concat(B.FirstName, B.LastName) as 'Nome do Chefe', A.EmployeeID ,concat(A.Firstname,A.LastName) as 'Nome do Funcionario'
+from Employees A 
+left join Employees B on A.ReportsTo = B.EmployeeID;
+
+
+-- 2 - quantas pessoas cada funcionário tem a seu cargo. 
+select EmployeeID as 'ID do chefe', concat(FirstName,LastName) as 'Nome do Chefe',ReportsTo,count(*) as Total
+from Employees 
+group by ReportsTo
+having ReportsTo = 2 or ReportsTo = 5 
