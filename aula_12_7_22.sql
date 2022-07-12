1f6e18d7efc6005ea9a05a83b216867d55dcbd49
Flávio Santos <96176081+Flaviolsantos@users.noreply.github.com>
2022-07-12 11:18:06 +0000

    Update aula_12_7_22.sql

--- aula_12_7_22.sql
+++ aula_12_7_22.sql
@@ -1,3 +1,42 @@
+select CategoryID,CategoryName,
+avg((select sum(UnitPrice)/ count(*) from Products B where B.CategoryID = A.CategoryID group by B.CategoryID)) as Média
+from Categories A
+group by CategoryID;
+
+
+
+
+select sum(UnitPrice)/ count(*) from Products B where CategoryID = 2
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+drop view if exists Temp_V1;
+create view Temp_V1 as
+select OrderID, count(*) as Quantidade
+from OrderDetails
+group by OrderID;
+
+select avg(Quantidade) from Temp_V1;
+drop view if exists Temp_V1;
+
+-------------------------------------------------------------------------------------------------------
+
+
+
+drop table if exists Temp_V1;
+create view Temp_V1 as
+select OrderID, count(*) as Quantidade
+from OrderDetails
+group by OrderID;
+
+select avg(Quantidade) from Temp_V1;
+-- drop view if exists Temp_V1;
+
+-----------------------------------------------------------------------------------------------------------
+
+
+
 -- 5 melhores clientes da emprasa
 
 
